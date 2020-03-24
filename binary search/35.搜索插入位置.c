5c7b8a3b7a77e8a64483e884a876178f59c7d220
林玮 <linw1995@icloud.com>
2020-03-24 23:46:06 +0800

    BinarySearch - c - 35 - Array

---
+++ binary search/35.搜索插入位置.c
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=c
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (44.34%)
+ * Likes:    460
+ * Dislikes: 0
+ * Total Accepted:    133.8K
+ * Total Submissions: 295K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ *
+ *
+ */
+
+// @lc code=start
+
+int searchInsert(int *nums, int numsSize, int target) {
+  int low = 0, high = numsSize, mid = (high + low) / 2;
+  while (low < high) {
+    if (nums[mid] < target) {
+      low = mid + 1;
+    } else if (nums[mid] > target) {
+      high = mid;
+    } else {
+      return mid;
+    }
+    mid = (high + low) / 2;
+  }
+  return mid;
+}
+
+// @lc code=end
